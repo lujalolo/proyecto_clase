e8c64c73d1a962724c002dffeb59194f66fd6578
PC23\daw20 <pfernandezsalas@safareyes.es>
2021-11-19 16:27:27 +0100

    Hemos añadido programa de sql

--- scripts.sql
+++ scripts.sql
@@ -1,8 +1,11 @@
 
 -- Ejemplo de create table
-
+CREATE TABLE bicicletas(
+    id int(10) not null auto_increment,
+);
 
 -- Ejemplo de consulta
-
+SELECT + from bicicletas
 
 -- Ejemplo de Alter table
+ALTER TABLE bicicletas AUTO_INCREMENT = 1
