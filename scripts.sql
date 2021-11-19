4b5d22a373330576043558fcc7108283ec0c9e4b
ljlopez <ljlopez@fundacionsafa.es>
2021-11-19 15:52:56 +0100

    He modificado el archivo scripts.sql

--- scripts.sql
+++ scripts.sql
@@ -1,15 +1,8 @@
-select id,nombre, precio, descuento, valoracion_total , url_imagen from pala;
-select * from jugador;
 
-delete from pala where id is not null ;
+-- Ejemplo de create table
 
-CREATE TABLE pala AUTO_INCREMENT=1;
 
-Select * from jugador where id = 4;
+-- Ejemplo de consulta
 
-SELECT * from pala;
 
-select 1;
-
-insert into jugador (ranking, nombre, nacionalidad, imagen, puntuacion)
-values (1,"Pepe", "España", "imagen_1", 900);
+-- Ejemplo de Alter table
