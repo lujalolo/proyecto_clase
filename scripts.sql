85e4b73ae3785c2793452e93751bb94f46bbf7be
lvaldearcos <valdearcos@outlook.com>
2021-11-19 16:35:45 +0100

    create table

--- scripts.sql
+++ scripts.sql
@@ -1,6 +1,11 @@
 
 -- Ejemplo de create table
-
+create table alumnos (
+    id int(10) auto-increment not null,
+    nombre varchar(100) not null,
+    apellidos varchar(150) not null,
+    constraint alumnos primary key (id)
+);
 
 -- Ejemplo de consulta
 
