9aa61bfd2455dcbeda6572e89d45cfa55b45240c
Jugador <dgavilanpedrozo@safareyes.es>
2021-11-19 16:30:47 +0100

    Commit 1 - Daniel Gavilán

--- ejercicio.py
+++ ejercicio.py
@@ -1,8 +1,10 @@
 
 #Ejercicio 1 -> Hacer metodo que reciba una cadena y la pinte por consola en minusculas
-
+def ejercicio1(string):
+    return string.lower()
 
 
 #Ejercicio 2 -> Hacer metodo que reciba una cadena y la pinte por consola en mayusculas
-
+def ejercicio1(string):
+    return string.upper()
 
