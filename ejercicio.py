32873e667893b85bad357023947c5113a776ec8e
Pedrorbarbeito <pedrorbarbeito02@gmail.com>
2021-11-19 16:36:25 +0100

    modificacion

--- ejercicio.py
+++ ejercicio.py
@@ -1,10 +1,6 @@
 
 #Ejercicio 1 -> Hacer metodo que reciba una cadena y la pinte por consola en minusculas
-create table (
-    id int(10)
-    nombre varchar (100),
-    apellidos varchar (100)
-);
+
 
 
 #Ejercicio 2 -> Hacer metodo que reciba una cadena y la pinte por consola en mayusculas
--- scripts.sql
+++ scripts.sql
@@ -1,6 +1,10 @@
 
 -- Ejemplo de create table
-
+create table (
+    id int(10)
+    nombre varchar (100),
+    apellidos varchar (100)
+);
 
 -- Ejemplo de consulta
 
