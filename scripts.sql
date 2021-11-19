d82dc9826290a2eb07b9d94302542784d99f65a1
DESKTOP-TIESGJV\celim <celimva@gmail.com>
2021-11-19 16:30:04 +0100

    crearte + consulta + alter

--- scripts.sql
+++ scripts.sql
@@ -1,8 +1,14 @@
 
 -- Ejemplo de create table
-
+create table alumno(
+id int(10),
+nombre varchar(140),
+dni varchar(10),
+primary key(id)
+);
 
 -- Ejemplo de consulta
-
+select nombre from alumnos;
 
 -- Ejemplo de Alter table
+alter table alumno rename to alumnado;
