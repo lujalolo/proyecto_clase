53b447e06881c5b40c63784d60c4a822087222f9
166Pedro <pgarciajimenez@safaresyes.es>
2021-11-19 16:29:34 +0100

    Creando tablas, consultando y alter

--- scripts.sql
+++ scripts.sql
@@ -1,8 +1,17 @@
 
 -- Ejemplo de create table
-
+create table talla(
+id int(10) auto_increment not null,
+descripcion varchar(100) not null,
+ancho_cuello double not null,
+largo_cuello_cintura double not null,
+constraint talla_pk primary key(id)
+);
 
 -- Ejemplo de consulta
 
-
+SELECT talla.idtalla, talla.descripcion, talla.ancho_cuello
+FROM talla;
 -- Ejemplo de Alter table
+
+alter table talla auto_increment=1;
