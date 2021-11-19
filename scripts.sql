1cb321a05bd4393249ea7dc0d124ea1bb69dc9e1
rubenmt <rmatiastiscar@safareyes.es>
2021-11-19 16:29:25 +0100

    Añadimos la creación de la tabla Peliculas y realizamos un SELECT

--- scripts.sql
+++ scripts.sql
@@ -1,8 +1,21 @@
 
 -- Ejemplo de create table
 
+    CREATE TABLE PELICULAS(
+	id int(10) auto_increment not null,
+	posicion int(10),
+	titulo varchar(100),
+	anio int(4),
+	valoraciones float,
+	sinopsis varchar(200),
+	imagenes varchar(200),
+	constraint id_pelicula primary key (id)
+);
+
 
 -- Ejemplo de consulta
 
+    SELECT posicion,titulo FROM PELICULAS
 
 -- Ejemplo de Alter table
+
