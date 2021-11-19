8db1512759eae4150480298554ab8300be60b4ba
rubenmt <rmatiastiscar@safareyes.es>
2021-11-19 16:32:48 +0100

    Creamos la tabla alumnos

--- scripts.sql
+++ scripts.sql
@@ -1,21 +1,16 @@
 
 -- Ejemplo de create table
 
-    CREATE TABLE PELICULAS(
-	id int(10) auto_increment not null,
-	posicion int(10),
-	titulo varchar(100),
-	anio int(4),
-	valoraciones float,
-	sinopsis varchar(200),
-	imagenes varchar(200),
-	constraint id_pelicula primary key (id)
-);
+    CREATE  table alumnos(
+        id int(10) auto_increment not null,
+        nombre varchar(100),
+        apellidos varchar(150)
+    );
 
 
 -- Ejemplo de consulta
 
-    SELECT posicion,titulo FROM PELICULAS
+
 
 -- Ejemplo de Alter table
 
