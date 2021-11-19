e2247ed0d34a41ea94a4eb40063613f3a2f3a2c3
PC33\daw20 <a1999afg@gmail.com>
2021-11-19 16:27:24 +0100

    He creado una tabla, he hecho una consulta y un alter

--- scripts.sql
+++ scripts.sql
@@ -1,8 +1,16 @@
 
 -- Ejemplo de create table
+CREATE TABLE Jugadores(
+    id int(10) auto_increment not null,
+    nombre varchar(50) not null,
+    primary key (id)
+);
 
 
 -- Ejemplo de consulta
-
+SELECT * from Jugadores nombre;
 
 -- Ejemplo de Alter table
+
+ALTER TABLE Jugadores
+ADD numero varchar(50) not null;
