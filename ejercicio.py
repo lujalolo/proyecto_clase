9bdbca678825d54b5d7ad59c4d6935c352e08c4e
ljlopez <ljlopez@fundacionsafa.es>
2021-11-19 15:52:15 +0100

    He modificado el archivo scripts.sql

--- ejercicio.py
+++ ejercicio.py
@@ -1,2 +1,8 @@
-def pintar_algo():
-    print("Hola")
+
+#Ejercicio 1 -> Hacer metodo que reciba una cadena y la pinte por consola en minusculas
+
+
+
+#Ejercicio 2 -> Hacer metodo que reciba una cadena y la pinte por consola en mayusculas
+
+
