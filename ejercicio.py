77c8868767a61604c78e7936836ab28f6f8a73e0
PC38\daw20 <rnietourena@safareyes.es>
2021-11-19 16:36:06 +0100

    hacemos tabla y ejercicio

--- ejercicio.py
+++ ejercicio.py
@@ -1,8 +1,12 @@
 
 #Ejercicio 1 -> Hacer metodo que reciba una cadena y la pinte por consola en minusculas
 
-
+def cadenamin(mensaje):
+    cad2 = mensaje.lower()
+    return cad2
 
 #Ejercicio 2 -> Hacer metodo que reciba una cadena y la pinte por consola en mayusculas
 
-
+def cadenamax(mensaje):
+    cad2 = mensaje.upper()
+    return cad2
