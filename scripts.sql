13d821c2814910d826815dd102fe060f5bc7825d
Cornejo <acornejolopez@safareyes.es>
2021-11-19 16:29:07 +0100

    Añadimos los elementos de ejemplo a scripts.sql

--- scripts.sql
+++ scripts.sql
@@ -1,8 +1,16 @@
 
 -- Ejemplo de create table
+create table ejercito (
+id int (10) auto_increment not null,
+infanteria int (100) not null,
+caballeria int (100) not null,
+arqueros int (100) not null,
+constraint ejercito_pk primary key (id)
+);
 
 
 -- Ejemplo de consulta
-
+select * from luchadores ;
 
 -- Ejemplo de Alter table
+alter table luchadores auto_increment = 1;
