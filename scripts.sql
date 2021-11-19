14320b2df4ee362b61341fc7a07d81a1c57c0ed8
ljlopez <ljlopez@fundacionsafa.es>
2021-11-19 16:35:22 +0100

    Version 1

--- scripts.sql
+++ scripts.sql
@@ -1,8 +1,13 @@
 
 -- Ejemplo de create table
-
+create table alumnos(
+    id int(10) auto_increment not null,
+    nombre varchar(100),
+    apellidos varchar(150),
+    curso int(1),
+);
 
 -- Ejemplo de consulta
-select * from alumno;
+
 
 -- Ejemplo de Alter table
